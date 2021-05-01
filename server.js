de02bd8807276363c16ffab3b67706dad927b2fb
kaveenakash2 <kaveenakashya@gmail.com>
2021-05-01 15:10:04 +0530

    Base Structure

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(8000,() =>{
+    console.log('Server Started')
+})
+
+
+
+
+
+
+
+
